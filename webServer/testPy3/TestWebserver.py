f4bbff40e70e3cc3e8ac2655daba773efde9870b
RetepRelleum <retep.relleum@bluewin.ch>
2020-12-06 07:48:09 +0100

    Test

--- webServer/testPy3/TestWebserver.py
+++ webServer/testPy3/TestWebserver.py
@@ -5,7 +5,7 @@ print(sys.path)
 from webServer import WebServer
 webserver =WebServer(sys.path[0]+'/webRoot',8080)
 
-webserver =WebServer('sd/webRoot',80)
+#webserver =WebServer('sd/webRoot',80)
 
 while True:
     pass 
